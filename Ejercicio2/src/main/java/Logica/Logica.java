6e613bf5e5be1b88509c21204be1a83be7918d3c
nerioferrebus <nerio.ferrebuscastillo@unicolombo.edo.co>
2023-09-13 16:39:13 -0500

    Empezando interfaces

---
+++ Ejercicio2/src/main/java/Logica/Logica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+import Igu.Ventana;
+ 
+
+public class Logica {
+    
+    public static void main(String[] args) {
+        
+        Ventana venta = new Ventana();
+        venta.setVisible(true);
+        venta.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
